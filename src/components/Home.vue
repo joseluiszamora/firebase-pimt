3fe9defc09834e1dcd8c9c805c60bc4fef9b01aa
jzamora <jzamora@miteleferico.bo>
2018-06-08 18:16:37 -0400

    dashboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,30 @@
 <template>
     <div>
         <v-toolbar
-                dense
-                color="primary"
-                dark
-                extended
-                extension-height="300"
-                flat
+					dense
+					color="primary"
+					dark
+					extended
+					extension-height="300"
+					flat
         >
             <v-toolbar-title class="mx-auto" slot="extension">
-                <span>Welcome to Vuebase</span>
-                <h2>Everything starts here.</h2>
+                <span>Bienvenido</span>
+                <h2>Administrador Aplicación piMT.</h2>
                 <v-layout row>
                     <v-flex wrap>
                         <v-btn outline large fab color="white" class="outine-2">
-                            <i class="fab fa-vuejs fa-3x"></i>
+                            <i class="fab fa-android fa-3x"></i>
                         </v-btn>
                     </v-flex>
                     <v-flex wrap>
                         <v-btn outline large fab color="white" class="outine-2">
-                            <i class="fab fa-node fa-3x"></i>
+                            <i class="fab fa-google fa-3x"></i>
                         </v-btn>
                     </v-flex>
                     <v-flex wrap>
                         <v-btn outline large fab color="white" class="outine-2">
-                            <i class="fab fa-laravel fa-2x"></i>
+                            <i class="fab fa-vuejs fa-2x"></i>
                         </v-btn>
                     </v-flex>
                 </v-layout>
@@ -38,16 +38,16 @@
                         <v-container fluid grid-list-lg>
                             <v-layout row wrap>
                                 <v-flex xs12>
-                                    <h2 class="white--text">Discover Vuebase</h2>
+                                    <h2 class="white--text">Secciones</h2>
                                 </v-flex>
                             </v-layout>
                             <v-layout row wrap>
-                                <v-flex xs12 sm6 md6 lg4 v-for="item in links" :key="item.id">
+                                <v-flex xs12 sm6 md6 lg3 v-for="item in links" :key="item.id">
                                     <v-card>
                                         <v-card-media
-                                                src=""
-                                                height="125px"
-                                                :class="item.color"
+											src=""
+											height="125px"
+											:class="item.color"
                                         >
                                         </v-card-media>
                                         <v-card-title primary-title>
@@ -59,9 +59,8 @@
                                             </div>
                                         </v-card-title>
                                         <v-card-actions>
-                                            <v-btn flat color="primary" class="learn-more-btn">Learn more</v-btn>
                                             <v-spacer></v-spacer>
-                                            <v-btn flat color="primary">Get started</v-btn>
+                                            <v-btn flat color="primary">ver</v-btn>
                                         </v-card-actions>
                                     </v-card>
                                 </v-flex>
@@ -75,51 +74,55 @@
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                links: [
-                    {
-                        id: '1',
-                        title: 'Dashboard',
-                        description: 'Get detailed anlytics to measure and analyze how users engage with your app',
-                        color: 'orange'
-                    },
-                    {
-                        id: '2',
-                        title: 'Storage',
-                        description: 'Store and retrieve user-generated content, such as images, audio, and videos, without server-side code.',
-                        color: 'teal'
-                    },
-                    {
-                        id: '3',
-                        title: 'Notifications',
-                        description: 'Manage your notification campaigns and send messages to reach the right users at the right time',
-                        color: 'blue'
-                    },
-                    {
-                        id: '4',
-                        title: 'Authentification',
-                        description: 'Authenticate and manage users from a variety of providers without server-side-code',
-                        color: 'purple'
-                    }
-                ]
-            }
+export default {
+  data() {
+    return {
+      links: [
+        {
+          id: "1",
+          title: "Inicio",
+          description:
+            "Sección Inicio.",
+          color: "orange"
+        },
+        {
+          id: "2",
+          title: "Usuarios",
+          description:
+            "Sección Usuarios.",
+          color: "teal"
+        },
+        {
+          id: "3",
+          title: "Mensajes",
+          description:
+            "Sección Mensajes.",
+          color: "blue"
+        },
+        {
+          id: "4",
+          title: "Avisos",
+          description:
+            "Sección Avisos.",
+          color: "purple"
         }
-    }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
-    .outine-2 {
-        border: 2px solid white;
-    }
+	.outine-2 {
+	border: 2px solid white;
+	}
 
-    .card--flex-toolbar {
-        margin-top: -104px;
-    }
+	.card--flex-toolbar {
+	margin-top: -104px;
+	}
 
-    .learn-more-btn {
-        text-transform: initial;
-        text-decoration: underline;
-    }
+	.learn-more-btn {
+	text-transform: initial;
+	text-decoration: underline;
+	}
 </style>
